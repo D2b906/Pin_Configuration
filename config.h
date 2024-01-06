fad18ede28f486bd3a3af133a80ac66047749ebe
d2b906 <d2b906@gmail.com>
2024-01-06 12:25:08 -0700

    Testing some Pins

--- config.h
+++ config.h
@@ -17,4 +17,12 @@
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+//# NO_ACTION_ONESHOT
+
+//#define MASTER_RIGHT
+//#define SOFT_SERIAL_PIN D3
+
+//#define MATRIX_ROWS 4
+//#define MATRIX_ROW_PINS { DD6, DD7, DD8, DD9}
+//#define MATRIX_COLS 12
+//#define MATRIX_COL_PINS { DD1 , DD4 , DD5, D7, D6, D5, D4, D2, D1, DD0 , DD2 , DD3 }
